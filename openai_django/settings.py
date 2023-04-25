bab97393c54491c25f5c846195de82bdc2d85050
Luis Aguilar <luisalberto.aguilar.gonzalez@gmail.com>
2023-04-25 13:19:09 -0600

    small changes

--- openai_django/settings.py
+++ openai_django/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 OPENAI_API_KEY = "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"
-print(OPENAI_API_KEY)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
